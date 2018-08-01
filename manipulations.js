b78680054d1076a7c5f193ec1844aa4422551a1f
Bianca Capretta <bianca.capretta@gmail.com>
2018-07-31 22:14:38 -0700

    Update manipulations.js

--- manipulations.js
+++ manipulations.js
@@ -8,9 +8,10 @@ var listOfMoney = [];
 
 var ui = SpreadsheetApp.getUi(); 
 var form = SpreadsheetApp.getActive();
-var doneCell = form.getRange('J2');
 var dataRange = form.getRange('D5:E14');
+var percentRange = form.getRange('D5:D14');
 var fixedRange = form.getRange('G5:G14');
+var done = form.getRange('J2');
 var start = form.getRange('E2').getValue();
 var end = form.getRange('F2').getValue();
 var salary = form.getRange('G2').getValue();
@@ -21,35 +22,34 @@ var yearly_salary;
 // Purpose: Copy all the entered percents from D5 to D14 into a list
 function copy() 
 {
-  for (i = 5; i < 15; i++) { // get values from cell and put into list
-    listOfPercent.push(form.getRange('D' + i).getValue());
-  }
+  listOfPercent = percentRange.getValues();
+  Logger.log("Percents: " + listOfPercent);
   Logger.log("About to check and convert");
   checkAndConvert();
 }
 
-// Purpose: to check if the total percents add to 100% and if so, to convert into effort in money
+// Purpose: to check if the total percents add to 100% or if the given dates are appropriate; if so, to convert into effort in money
 function checkAndConvert() 
 {
   var totalPercent = form.getRange('D15').getValue();
   
   if (salary == '') {
     ui.alert('You need to enter a Salary.');
-    doneCell.setValue('No');
   }
   else if (start == '' || end == '') {
-    doneCell.setValue('No');
     ui.alert('You must enter a start and end date.');
   }
   else if (totalPercent != 1) {
-    doneCell.setValue('No');
     // ******* could possibly offer a suggestion here to fix it! *********
     ui.alert('You need to enter amounts that add to 100%.');
   }
+  else if (start > end) {
+    ui.alert('You end date is before your start date. Please enter an appropriate timeline');
+  }
   else {
     convertToMoney();
-    doneCell.setValue('Yes');
-    ui.alert('You are ready to edit numbers in Percent Allocated and Money Allocated to see adjustments.');
+    done.setValue('Yes');
+    ui.alert('You are ready to edit numbers in Percent Allocated % and Yearly Rate $ to see adjustments.');
   }
 }
 
@@ -110,7 +110,7 @@ function reset()
   form.getRange('B5:C14').setValue(''); // project/grant names
   dataRange.setValue(0); // percent effort and money effort
   fixedRange.setValue('No'); // Fixed? column gets set to No
-  doneCell.setValue('No');
+  done.setValue('No');
 }
 
 // --------------------------------------------------EDIT on TRIGGER--------------------------------------------------
@@ -118,14 +118,17 @@ function reset()
 // Purpose: when a cell is edited, take the cell's new value and adjust 
 //          other cells so that the total percent of all projects remains at 100%
 function onEdit(e) 
-{
-  var ready = form.getRange('J2').getValue();
-  
-  // only do stuff to cells between D5 and E14 and if user has inputted info and pressed done
-  if (withinRange(e) && ready == 'Yes')
-  { 
-    getEffort(); // gets current values in both columns and puts into the lists
-    adjustValues(e.value, e.oldValue, e.range.getRow() - 5, e.range.getColumn() - 4);
+{ 
+  // if user tries to edit something where everything else is fixed, return value to old value
+  if (withinRange(e) && done.getValue() == 'Yes') {
+    if (numberUnfixed() <= 1) {
+      ui.alert('You cannot edit this cell since all other grants are fixed');
+      e.range.setValue(e.oldValue);
+    }
+    else { // only do stuff to cells between D5 and E14 and if user has inputted info and pressed done
+      getEffort(); // gets current values in both columns and puts into the lists
+      adjustValues(e.value, e.oldValue, e.range.getRow() - 5, e.range.getColumn() - 4);
+    }
   }
 }
 
@@ -162,6 +165,23 @@ function getEffort()
   } 
 }
 
+// Purpose: to count the number of fixed grants marked by user
+function numberUnfixed()
+{
+  var fixedRangeValues = fixedRange.getValues();
+  var dataRangeValues = dataRange.getValues();
+  Logger.log('Data!' + dataRangeValues);
+  var lastVal = fixedRange.getLastRow();
+  var noCount = 0;
+  
+  for (i = 0; i <= lastVal; i++) {
+    if (fixedRangeValues[i] == 'No' && dataRangeValues[i][0] > 0) { // only edit cells that are not fixed 
+      noCount++;
+    }
+  }
+  return noCount;
+}
+
 // Purpose: given an edited cell, consider the change (postive or negative) and respectively add or 
 //          remove the changed content from the rest of the cells; update all other cells accordingly
 // Arguments: editted value, previous value, the cell row that was edited (from 0-10) and the cell column the was edited (0-1)
@@ -172,25 +192,23 @@ function adjustValues(newVal, oldVal, cellRow, cellCol)
   
   // edit cells so that they add up to 100% again or so that the total money adds to the budget amount
   if (form.getRange('D15').getValue() != 1.0 || form.getRange('E15').getValue() != yearly_salary) { 
-    
     // get sum of all the percents besides the edited cell so value of change can be appropriately distributed to the rest of the colum
     var sum = getPartialSum(cellRow, cellCol); // get sum of editted column (all the percent efforts OR money effort) besides the edited cell
-   
-    doMath(diff, cellRow, cellCol, sum);
+    distributeChange(diff, cellRow, cellCol, sum);
     
     if (cellCol == 0) convertToMoney();
     if (cellCol == 1) convertToPercent();
   }
   
   if (form.getRange('D15').getValue() != 1.0) {
-    //checkRoundErrors(); 
+    // checkRoundErrors(); 
   }
 }
 
 // Purpose: take the difference of the editted value and distribute it among the other cells; checks if either column (percent effort or money effort) is editted
 // Alg: (percent effort / [sum of whole column - edited cell] ) 
 // *NOTE*: higher priority projects get more when distributed (not distributed evenly)
-function doMath(diff, cellRow, cellCol, sum) 
+function distributeChange(diff, cellRow, cellCol, sum) 
 {
   // algorithm favors the projects that already have more effort invested in them (higher effort * diff > lower effort * diff)
   for (i = 0; i < 10; i++) {
